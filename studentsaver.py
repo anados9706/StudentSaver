b1cbee9881c610f5c6cffef7689feb0a94295af8
Vaz <cvaz@acsicorp.com>
2022-04-12 19:17:06 -0700

    Please enter the commit message for your changes. Lines starting

--- studentsaver.py
+++ studentsaver.py
@@ -1,3 +1,4 @@
+
 class Student:
     def __init__(self, name, id, gpa):
         self.name = name
@@ -5,7 +6,31 @@ class Student:
         self.gpa = gpa
 
 def createStudent():
+    name = input("Please enter Name")
     id = input("Please enter student id")
-    input("Please enter student id")
-    
-#hello
+    gpa = input("Please enter GPA")
+    new_student = Student(name,id,gpa)
+    #TODO: Save on data.txt
+
+def readStudent():
+    #TODO: Read Student
+    print('')
+def updateStudent():
+    #TODO: Update Student
+    print('')
+def deleteStudent():
+    #TODO: Delete Student
+    print('')
+
+
+while True:
+    user_input = input("Choose: Create, Read, Update, Delete, Exit\n")
+    if user_input == 'Create':
+        createStudent()
+    elif user_input == 'Read':
+        readStudent()
+    elif user_input == 'Exit':
+        break
+    else :
+        print("Invalid command, please try again")
+

bcfc65650714828dc2773dcdf7c8fae198ba642c
porterHealy <portheal9586@granitesd.org>
2022-04-13 09:32:05 -0600

    My Comit

--- studentsaver.py
+++ studentsaver.py
@@ -1,4 +1,3 @@
-
 class Student:
     def __init__(self, name, id, gpa):
         self.name = name
@@ -18,10 +17,12 @@ def readStudent():
 def updateStudent():
     #TODO: Update Student
     print('')
-def deleteStudent():
-    #TODO: Delete Student
-    print(' ')
-
+def deleteStudent():               #PORTER
+    f = open("data.txt", "r+")
+    f.seek(0)
+    f.truncate()
+    print("Data successfully deleted!")
+    #TODO: SAVE DATA INTO TXT FILE
 
 while True:
     user_input = input("Choose: Create, Read, Update, Delete, Exit\n")
@@ -29,6 +30,8 @@ while True:
         createStudent()
     elif user_input == 'Read':
         readStudent()
+    elif user_input == "Delete":
+        deleteStudent()
     elif user_input == 'Exit':
         break
     else :

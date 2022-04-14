e821912c5f7f3011db35ccb3584ef099d636c78b
Marina <mariscot9584@gmail.com>
2022-04-14 09:56:23 -0600

    Read

--- studentsaver.py
+++ studentsaver.py
@@ -6,21 +6,39 @@ class Student:
         self.gpa = gpa
 
 def createStudent():
-    name = input("Please enter Name")
-    id = input("Please enter student id")
-    gpa = input("Please enter GPA")
+    name = input("Please enter Name ")
+    id = input("Please enter student id ")
+    gpa = input("Please enter GPA ")
     new_student = Student(name,id,gpa)
-    #TODO: Save on data.txt
+    with open("data.txt", "a+") as data:
+        data.write(id + "\n")
+        data.write(name + "\n")
+        data.write(gpa + "\n")
 
-def readStudent():
-    #TODO: Read Student
-    print('')
+def readStudent():              #Marina
+    id = input("Please enter student id ")
+    myfile = open("data.txt", "r")
+    contents = myfile.readlines()
+    i=0
+    while i < len(contents):
+        if contents[i].replace("\n", "")==id:
+            print(contents[i])
+            print(contents[i+1])
+            print(contents[i+2])
+        myfile.close()
+        i+=3
+    
+    
+
 def updateStudent():
     #TODO: Update Student
     print('')
-def deleteStudent():
-    #TODO: Delete Student
-    print(' ')
+
+def deleteStudent():               #PORTER
+    f = open("data.txt", "r+")
+    f.seek(0)
+    f.truncate()
+    print("Data successfully deleted!")
 
 
 while True:
@@ -29,6 +47,8 @@ while True:
         createStudent()
     elif user_input == 'Read':
         readStudent()
+    elif user_input == "Delete":
+        deleteStudent()
     elif user_input == 'Exit':
         break
     else :
